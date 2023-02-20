dd5a9b88c54e0bcb5f8567184e5b458128702a11
tuphan-dn <tuphan@descartes.network>
2023-02-20 21:27:19 +0700

    bugfix

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,9 +4,7 @@ export default function Footer() {
   return (
     <Row gutter={[24, 24]} style={{ marginTop: 64 }}>
       <Col span={24}>
-        <Typography.Text type="secondary">
-          Desig © 2023, All Rights Reserved.
-        </Typography.Text>
+        <p style={{ opacity: 0.5 }}>Desig © 2023, All Rights Reserved.</p>
       </Col>
     </Row>
   )
