37a8f6d5a38714f69060d0cd82610cddb26fdaff
tuphan-dn <tuphan@descartes.network>
2023-04-12 17:52:12 +0700

    debug

--- next.config.js
+++ next.config.js
@@ -1,21 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath,
-  assetPrefix,
+  basePath: '/desig-next',
+  assetPrefix: '/desig-next/',
 }
 
 module.exports = nextConfig
