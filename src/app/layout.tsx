1dace5b754d973ba28d7d288a6e5f1ed2d4e38e4
tuphan-dn <tuphan@descartes.network>
2023-04-15 01:08:37 +0700

    ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           src="https://www.googletagmanager.com/gtag/js?id=G-R91HV8NWFY"
           strategy="afterInteractive"
         />
-        <Script>
+        <Script id="ga">
           {`window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
