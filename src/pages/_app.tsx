e9573d96389934bd909f6f96ea267bff237d1878
minhcong <83624415+dmcong@users.noreply.github.com>
2023-04-25 16:13:37 +0700

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import { AppProps } from 'next/app'
+import { useState } from 'react'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
+
+export default function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{
+  initialSession: Session
+}>) {
+  // Create a new supabase browser client on every first render.
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  )
+}
