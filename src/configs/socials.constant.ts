2be00996a4dfe5d1c4d7581204f21b67d109b1cc
tuphan-dn <tuphan@descartes.network>
2023-04-25 16:00:44 +0700

    rename

--- src/configs/socials.constant.ts
+++ src/configs/socials.constant.ts
@@ -3,6 +3,6 @@ export const email = 'mailto:hi@desig.io'
 export const twitter = 'https://twitter.com/DesigLabs'
 export const github = 'https://github.com/Desig-Labs'
 export const pitchdeck =
-  'https://www.canva.com/design/DAFXv84yDsE/KAVpFvOMa7H-mNOGlJh3Ng/edit?utm_content=DAFXv84yDsE&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton'
+  'https://www.canva.com/design/DAFXv84yDsE/KAVpFvOMa7H-mNOGlJh3Ng/view'
 export const chromestore =
   'https://chrome.google.com/webstore/detail/desig-wallet/panpgppehdchfphcigocleabcmcgfoca?hl=en-US'
