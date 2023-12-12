dde66e2bbc6460209919ee46188dd9166c1f61de
quanhoang882002 <135295387+hnquan8@users.noreply.github.com>
2023-12-12 17:08:18 +0700

    fix change text desc

--- src/app/wallet/moreFeature/index.tsx
+++ src/app/wallet/moreFeature/index.tsx
@@ -20,7 +20,7 @@ const LIST_FEATURE = [
   },
   {
     title: 'Staking Aggregator',
-    desc: 'Maximize your staking potential with Desig‘s all-in-one aggregator. Simplify your investment and tracking of platforms and validators.',
+    desc: 'Maximize your staking potential with Desig’s all-in-one aggregator. Simplify your investment and tracking of platforms and validators.',
     img: STAKING,
   },
   {
