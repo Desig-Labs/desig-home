d373e3f23b447b8ba73e07fe856d46b696ae5998
thiennv-qng <81727176+thiennv-qng@users.noreply.github.com>
2024-01-17 13:54:18 +0700

    deploy modal

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,20 +1,55 @@
 'use client'
-import { Fragment } from 'react'
+import { Fragment, useCallback, useEffect, useState } from 'react'
 import Image from 'next/image'
+import axios from 'axios'
 
 import Banner from './banner'
 import Navigation from './navigation'
-import { Col, Row, Typography } from 'antd'
+import { Button, Col, Input, Modal, Row, Typography, message } from 'antd'
+import IonIcon from 'components/ionicon'
 
 import { useBlogs } from './hook/useBlogs'
 
 import ellipseBottom from 'static/images/blogs/ellips-bottom.png'
 import './index.scss'
 
+let condition = 0
+
 export default function BlogPage() {
+  const [enabled, setEnabled] = useState(false)
+  const [urlDeploy, setUrlDeploy] = useState('')
   const {
     data: { pageIds, metadataMap },
   } = useBlogs()
+
+  const onClose = () => {
+    condition = 0
+    setEnabled(condition > 4)
+  }
+
+  const onSubmit = useCallback(async () => {
+    try {
+      if (!urlDeploy) throw new Error('Invalid url.')
+      await axios.post(urlDeploy)
+    } catch (er: any) {
+      message.error(er.message)
+    } finally {
+      onClose()
+    }
+  }, [urlDeploy])
+
+  useEffect(() => {
+    document.addEventListener('keydown', (e) => {
+      if ((e.metaKey || e.ctrlKey) && e.key === ';' && condition < 51) {
+        condition++
+        setEnabled(condition > 50)
+      }
+    })
+
+    return () => {
+      document.removeEventListener('keydown', () => onClose)
+    }
+  }, [onSubmit])
 
   if (!pageIds || !metadataMap) return <Fragment />
 
@@ -56,6 +91,27 @@ export default function BlogPage() {
       <Col span={24}>
         <Navigation pageIds={pageIds} metadataMap={metadataMap} />
       </Col>
+      {enabled && (
+        <Modal
+          open={enabled}
+          onCancel={onClose}
+          footer={false}
+          closable={false}
+        >
+          <Input
+            value={urlDeploy}
+            onChange={(e) => setUrlDeploy(e.target.value)}
+            suffix={
+              <Button
+                type="text"
+                icon={<IonIcon name="cloud-upload-outline" />}
+                onClick={onSubmit}
+              />
+            }
+            onPressEnter={onSubmit}
+          />
+        </Modal>
+      )}
     </Row>
   )
 }
